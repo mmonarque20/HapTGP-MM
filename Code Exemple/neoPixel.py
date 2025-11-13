1ea387f04b4d90f85b199ac3bf3e11ec9d633b52
mmonarque20 <marianne.monarque@videotron.ca>
2025-11-13 11:09:19 -0500

    modification du code
    Les dels se s'éteignent lorsqu'on ferme le programme

--- Code Exemple/neoPixel.py
+++ Code Exemple/neoPixel.py
@@ -1,12 +1,22 @@
-#Code exemple pour tester le fonctionnement des dels
-
 import board
 import neopixel
+import time
 
 pixels = neopixel.NeoPixel(board.D21, 12, pixel_order=neopixel.RGB, auto_write=False)
 
-# Mauve = mélange de rouge et bleu
-pixels.fill((0, 50, 50))  # G, R, B
+try:
+    # Allume les DELs en mauve
+    pixels.fill((0, 50, 50))  # G, R, B
+    pixels.show()
 
-pixels.show()
+    # Laisse allumé pendant 5 secondes
+    time.sleep(5)
 
+except KeyboardInterrupt:
+    print("Arrêt manuel du programme.")
+
+finally:
+    # Éteint les DELs proprement
+    pixels.fill((0, 0, 0))
+    pixels.show()
+    print("DELs éteintes.")
