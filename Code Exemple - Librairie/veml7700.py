6c174b81398c71fd3e9ccd43b085ad2c59875098
mmonarque20 <marianne.monarque@videotron.ca>
2025-11-16 11:54:35 -0500

    Update veml7700.py
    Création librairie à partir du code exemple afin de pouvoir l'utiliser dans d'autres programmes. Ce programme permet aussi de tester le module

--- Code Exemple - Librairie/veml7700.py
+++ Code Exemple - Librairie/veml7700.py
@@ -1,12 +1,11 @@
-#Code exemple pour tester le fonctionnement du VEML7700
+""" Librairie et Code exemple pour tester  le fonctionnement du Veml7700 et 
+l'utiliser dans d'autres programmes"""
 
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
 import time
-
 import board
-
 import adafruit_veml7700
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -14,6 +13,11 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 veml7700 = adafruit_veml7700.VEML7700(i2c, address=0x10)
 time.sleep(0.5) # laisser le capteur s'initialiser
 
-while True:
-    print("Ambient light: ", veml7700.light, "lux")
-    time.sleep(2)
+def getLuminosite():
+    return veml7700.light
+    
+
+if __name__ == "__main__":
+    while True:
+        print("Ambient light: ", veml7700.light, "lux")
+        time.sleep(2)
