3fbc599f5dbbc36c424cfec96d0092a5fb7d3872
mmonarque20 <marianne.monarque@videotron.ca>
2025-11-20 14:30:04 -0500

    Update horloge.py
    Création de fonctions et séparation de la date et de l'heure

--- Code Exemple - Librairie/horloge.py
+++ Code Exemple - Librairie/horloge.py
@@ -1,18 +1,34 @@
-#Code exemple pour tester le fonctionnement du ds3231
+#Code exemple pour tester le fonctionnement du ds3231 (Affichage de la date et heure dans le terminal)
 
 import adafruit_ds3231
 import time
 
 import board
 
-i2c = board.I2C() #board.SCL, board.SDA
+def init_horloge():
+    i2c = board.I2C() #board.SCL, board.SDA
+    rtc = adafruit_ds3231.DS3231(i2c)
+    rtc.datetime = time.localtime()
+    return rtc
 
-rtc = adafruit_ds3231.DS3231(i2c)
+def get_date(rtc):
+    while True:
+        t = rtc.datetime
+        return(f"{t.tm_mday}/{t.tm_mon}/{t.tm_year}")
+       
+def get_heure(rtc):
+    while True:
+        t = rtc.datetime
+        return(f"{t.tm_hour}:{t.tm_min}:{t.tm_sec}")
+        
 
-rtc.datetime = time.localtime()
 
-while True:
-    t = rtc.datetime
-    print(f"{t.tm_mday}/{t.tm_mon}/{t.tm_year} - {t.tm_hour}:{t.tm_min}:{t.tm_sec}")
-    time.sleep(1)
-
+if __name__ == "__main__":
+    rtc = init_horloge()
+    print(get_date(rtc))
+    try:
+        while True:
+            print(get_heure(rtc))
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Arrêt du test horloge.")
